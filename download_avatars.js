1649d6931644bdc62fa39f59dc25393c26735b51
moeenah <moeen.ahmad97@hotmail.com>
2018-05-14 23:26:37 +0000

    fixed indentation and re-organized function placement

--- download_avatars.js
+++ download_avatars.js
@@ -12,7 +12,7 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //takes api from github
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
-    //
+
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request'
@@ -24,43 +24,43 @@ function getRepoContributors(repoOwner, repoName, cb) {
   });
 }
 
-//takes name of owner and repo
-getRepoContributors(owner, repo, function(err, result) {
-    //check if either owner or repo is undefined and return error
-    if (owner === undefined || repo === undefined) {
-      console.log("Enter an owner AND a repo name!");
-    }
-    //if both owner and repo are ok, function continues
-    else {
-    //creates folder named using 'repo'
-    var path = "./avatars/" + repo;
-    fs.mkdir(path);
-
-    console.log("Errors:", err);
-    var obj = JSON.parse(result);
-
-    var avatarURL = '';
-    var loginPath = '';
-
-    for (var i in obj) {
-      avatarURL = obj[i].avatar_url;
-      loginPath = "avatars/" + repo + "/" + obj[i].login + ".jpg";
-      downloadImageByURL(avatarURL, loginPath);
-      }
-   }
-
-  });
-
 //downloads image
 function downloadImageByURL(url, filePath) {
   request.get(url)
-       .on('error', function (err) {
-         throw err;
-       })
-       .on('response', function (response) {
-         console.log('Response Status Code: ', response.statusCode);
-         console.log('Download complete');
-       })
-       .pipe(fs.createWriteStream(filePath));
-       console.log('Downloading image...');
+   .on('error', function (err) {
+     throw err;
+   })
+   .on('response', function (response) {
+     console.log('Response Status Code: ', response.statusCode);
+     console.log('Download complete');
+   })
+   .pipe(fs.createWriteStream(filePath));
+   console.log('Downloading image...');
   }
+
+//takes name of owner and repo
+getRepoContributors(owner, repo, function(err, result) {
+  //check if either owner or repo is undefined and return error
+  if (owner === undefined || repo === undefined) {
+    console.log("Enter an owner AND a repo name!");
+  }
+  //if both owner and repo are ok, function continues
+  else {
+  //creates folder named using 'repo'
+  var path = "./avatars/" + repo;
+  fs.mkdir(path);
+
+  console.log("Errors:", err);
+  var obj = JSON.parse(result);
+
+  var avatarURL = '';
+  var loginPath = '';
+
+  for (var i in obj) {
+    avatarURL = obj[i].avatar_url;
+    loginPath = "avatars/" + repo + "/" + obj[i].login + ".jpg";
+    downloadImageByURL(avatarURL, loginPath);
+    }
+  }
+
+});
