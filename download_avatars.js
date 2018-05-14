a4796ae5258d074e491c7ba5c515b139afc30609
moeenah <moeen.ahmad97@hotmail.com>
2018-05-14 18:14:57 +0000

    implemented token request

--- download_avatars.js
+++ download_avatars.js
@@ -1,12 +1,21 @@
 var request = require('request');
+var secrets = require('secrets');
 
 console.log('Welcome to the GitHub Avatar Downloader!');
 
 function getRepoContributors(repoOwner, repoName, cb) {
-  var url = "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors";
-  request(url, function(err, res, body) {
+  var options = {
+    url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
+    headers: {
+      'User-Agent': 'request'
+    }
+  };
+
+  request(authorization, options, function(err, res, body) {
     cb(err, body);
   });
+
+
 }
 
 getRepoContributors("jquery", "jquery", function(err, result) {
