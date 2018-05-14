8c8eafd0d84ec17ab980810f3f935132fa991817
moeenah <moeen.ahmad97@hotmail.com>
2018-05-14 20:47:26 +0000

    folder is created with name using input recorded from 'repo'

--- download_avatars.js
+++ download_avatars.js
@@ -1,6 +1,9 @@
+//require various functoins
 var request = require('request');
 var secrets = require('./secrets');
 var fs = require('fs');
+
+//assign variables to CLI arguments
 var owner = process.argv[2];
 var repo = process.argv[3];
 
@@ -9,6 +12,7 @@ console.log('Welcome to the GitHub Avatar Downloader!');
 //takes api from github
 function getRepoContributors(repoOwner, repoName, cb) {
   var options = {
+    //
     url: "https://api.github.com/repos/" + repoOwner + "/" + repoName + "/contributors",
     headers: {
       'User-Agent': 'request'
@@ -22,11 +26,16 @@ function getRepoContributors(repoOwner, repoName, cb) {
 
 //takes name of owner and repo
 getRepoContributors(owner, repo, function(err, result) {
+    //check if either owner or repo is undefined and return error
     if (owner === undefined || repo === undefined) {
       console.log("Enter an owner AND a repo name!");
     }
-
+    //if both owner and repo are ok, function continues
     else {
+    //creates folder named using 'repo'
+    var path = "./avatars/" + repo;
+    fs.mkdir(path);
+
     console.log("Errors:", err);
     var obj = JSON.parse(result);
 
@@ -35,7 +44,7 @@ getRepoContributors(owner, repo, function(err, result) {
 
     for (var i in obj) {
       avatarURL = obj[i].avatar_url;
-      loginPath = "avatars/" + obj[i].login + ".jpg";
+      loginPath = "avatars/" + repo + "/" + obj[i].login + ".jpg";
       downloadImageByURL(avatarURL, loginPath);
       }
    }
@@ -54,4 +63,4 @@ function downloadImageByURL(url, filePath) {
        })
        .pipe(fs.createWriteStream(filePath));
        console.log('Downloading image...');
-}
+  }
